a3fdc0a0b2a2b358be39608c319659e5ca5b2565
Dinuka7L <dinoo.walker7@gmail.com>
2025-08-03 21:16:48 +0530

    aaaaa

--- api/getQuizzes.js
+++ api/getQuizzes.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     if (req.method === 'OPTIONS') {
         res.status(200).end();
+        console.log('CORS preflight request handled');
         return;
     }
     if (req.method !== 'GET') {
@@ -40,6 +41,7 @@ export default async function handler(req, res) {
             category: quiz.category,
             questionCount: quiz.questions?.length || 0
         }));
+        console.log('Fetched quizzes:', quizSummaries);
         res.status(200).json(quizSummaries);
     }
     catch (error) {
